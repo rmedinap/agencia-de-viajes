6ebc2c1d3b21a4afb0b76fb03f41d93c208f3c9c
Ricardo <hammett87@hotmail.com>
2012-11-08 01:14:00 -0500

    working home features

--- features/home.feature
+++ features/home.feature
@@ -3,9 +3,16 @@
 Característica: los usuarios deberían de ver los últimos items de cada sección en la página inicial
 
 Antecedentes: existe un item de cada una de las secciones
-  Cuando existe el Slide "slide.jpg"
+  Cuando estoy en la página inicial
+  Y existe el slide "slide.jpg"
+  Y existe el viaje "Machu Picchu"
+  Y existe la noticia "Feriado largo generará millones por actividad turística"
+  Y existe el paquete con "Novedad de Apertura"
 
-Escenario: los visitantes navegan por el landing page
-  Dado que estoy en la página inicial
+Escenario: los visitantes navegan en la página inicial
   Entonces debería ver la imagen "slide.jpg"
-
+  Y debería ver el slide "slide.jpg"
+  Y debería ver el viaje "Machu Picchu"
+  Y debería ver la noticia "Feriado largo generará millones por actividad turística"
+  Y debería ver el paquete con "Novedad de Apertura"
+  Y no debería ver los controles de administración
---
+++ features/steps_definitions/home_steps.rb
@@ -0,0 +1,34 @@
+Cuando /^estoy en la página inicial$/ do
+end
+
+Cuando /^existe el slide "(.*?)"$/ do |slide|
+  slide = Slide.find_by_name(slide)
+end
+
+Cuando /^existe el viaje "(.*?)"$/ do |viaje|
+  viaje = Viaje.find_by_name(viaje)
+end
+
+Cuando /^existe la noticia "(.*?)"$/ do |noticia|
+  noticia = Noticia.find_by_name(noticia)
+end
+
+Cuando /^existe el paquete con "(.*?)"$/ do |paquete|
+  paquete = Paquete.find_by_name(paquete)
+end
+
+Entonces /^debería ver el slide "(.*?)"$/ do |slide|
+  page.should have_content("slide.jpg")
+end
+
+Entonces /^debería ver el viaje "(.*?)"$/ do |viaje|
+  page.should have_content( "Machu Picchu" + viaje)
+end
+
+Entonces /^debería ver la noticia "(.*?)"$/ do |noticia|
+  page.should have_content("Feriado largo generará millones por actividad turística" + noticia)
+end
+
+Entonces /^debería ver el paquete con "(.*?)"$/ do |paquete|
+  page.should have_content("Novedad de Apertura" + paquete)
+end
