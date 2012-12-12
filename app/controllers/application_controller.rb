b4ab960474ec16cecf8b36180d03e6ffcb84a6f6
Ricardo <hammett87@hotmail.com>
2012-12-11 23:47:56 -0500

    Worked Slides

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def bodyid
+    @bodyid = params[:controller].parameterize
+  end
+
+  def bodyclass
+    @bodyclass = params[:action].parameterize
+
+    if params[:controller] == 'home'
+      @front = 'front'
+    else
+      @front = 'not-front'
+    end
+
+    if current_admin
+      @admin = 'infinity-admin'
+    else
+      @admin = ''
+    end
+
+    if current_user
+      @user = 'logged-in'
+    else
+      @user = 'user-not-logged-in'
+    end
+
+    if File.exists?(Rails.root.join("app", "views", params[:controller], "_sidebar_first.html.haml"))
+      @sidebar = 'sidebar sidebar-first'
+    else
+      @sidebar = ''
+    end
+
+    @bodyclass = @bodyclass + ' ' + @front + ' ' + @admin + ' ' + @user + ' ' + @sidebar
+  end
+
+  helper_method :bodyid
+  helper_method :bodyclass
+
 end
--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,2 +1,15 @@
 class SlidesController < InheritedResources::Base
+  before_filter :authenticate_admin!
+
+  def create
+    create! { slides_path }
+  end
+ 
+  def update
+    update! { slides_path }
+  end
+
+  def destroy
+    destroy! { slides_path }
+  end
 end
