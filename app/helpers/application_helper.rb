7755967235ea6f197e8ce4d965a338180b9ad191
Ricardo <hammett87@hotmail.com>
2012-12-15 18:19:56 -0500

    Super clean menu with application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,21 @@
 module ApplicationHelper
 
-  def lavalamp(current_controller)
-    if request.path_parameters[:controller] == "#{current_controller}"
-      puts 'selectedLava'
+  # list menu maker
+  def menu_li(current_page, current_controller, current_url)
+    content_tag(:li, :class => lavalamp(current_controller)) do
+      link_to_unless_current current_page, current_url do
+        content_tag(:span, current_page)
+      end
     end
   end
+
+  # extension of menu maker wich applies lavalamp class to the current controller
+  def lavalamp(current_controller)
+    if params[:controller] == "#{current_controller}"
+      'selectedLava'
+    end
+  end
+
 
 
   def paragraph(post, length)
