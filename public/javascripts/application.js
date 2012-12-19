b27cde2edc778e3f61972f6b243d6f0393179a36
Ricardo <hammett87@hotmail.com>
2012-12-18 22:21:23 -0500

    Fixed slider functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,83 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+/*
+==================================================================
+HOME SLIDER
+==================================================================
+*/
+
+$(document).ready(function($) {
+
+    $('.royalSlider').royalSlider({
+      autoPlay: {
+      // autoplay options go gere
+      enabled: true,
+      pauseOnHover: true,
+      stopAtAction: false
+    },
+    block: {
+    // animated blocks options go gere
+      fadeEffect: false,
+      moveEffect: 'top',
+      delay: 500,
+      speed: 150,
+      easing:'easeOutSine'
+    },
+    arrowsNav: true,
+    allowCSS3: true,
+    arrowsNavAutoHide: false,
+    fadeinLoadedSlide: false,
+    controlNavigationSpacing: 0,
+    controlNavigation: 'bullets',
+    imageScaleMode: 'none',
+    imageAlignCenter:false,
+    blockLoop: true,
+    loop: true,
+    numImagesToPreload: 6,
+    transitionType: 'move',
+    keyboardNavEnabled: true,
+    sliderDrag: true,
+    sliderTouch: true,
+    keyboardNavEnabled: true,
+    });
+
+  // PARTNERS
+    $('ul.slider').royalSlider({
+      autoPlay: {
+      // autoplay options go gere
+      enabled: true,
+      pauseOnHover: true,
+      stopAtAction: false,
+      delay: 6000,
+    },
+    block: {
+    // animated blocks options go gere
+      fadeEffect: false,
+      moveEffect: 'bottom',
+      delay: 500,
+      speed: 150,
+      easing:'easeOutQuad'
+    },
+    transitionDuration:2000,
+    arrowsNav: true,
+    allowCSS3: true,
+    arrowsNavAutoHide: false,
+    fadeinLoadedSlide: false,
+    controlNavigationSpacing: 0,
+    imageScaleMode: 'none',
+    imageAlignCenter:false,
+    blockLoop: true,
+    loop: true,
+    numImagesToPreload: 6,
+    transitionType: 'move',
+    keyboardNavEnabled: true,
+    sliderDrag: true,
+    sliderTouch: true,
+    keyboardNavEnabled: true,
+    slidesOrientation: 'vertical',
+    });
+
+});
+
