38f4c2c83ecbe461d30d01fbbb38637742c31706
Ricardo <hammett87@hotmail.com>
2013-01-08 20:41:44 -0500

    working

--- app/controllers/posts_and_events_controller.rb
+++
@@ -1,4 +0,0 @@
-class PostsAndEventsController < InheritedResources::Base
-belongs_to :post, :finder => :find_by_slug!
-
-end
--- app/models/posts_and_event.rb
+++
@@ -1,7 +0,0 @@
-class PostsAndEvent < ActiveRecord::Base
-belongs_to :post
-
-extend FriendlyId
-friendlyid :title, :use => :slugged
-
-end
