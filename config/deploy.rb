d6ed32a019d5332ea8228cef944a4a859c14f530
Rafael Medina <rmedinap@gmail.com>
2014-01-27 15:32:28 -0500

    server ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ require "rvm/capistrano"
 set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :user  # Don't use system-wide RVM
 
-server "rubyonrailsperu.com", :web, :app, :db, primary: true
+server "infinityperu.com", :web, :app, :db, primary: true
 
 set :application, "infinity"
 set :user, "paul"
