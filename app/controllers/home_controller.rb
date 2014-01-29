c3ecbe0e24bd6c86351a755e7cb941a59ecb25e9
Jesus <jesusalvarez@gmail.com>
2014-01-29 16:56:39 -0500

    Arreglados los testimonios en el inicio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @about_infinity = About.limit(1)
     @last_post = Post.limit(1).where(:landing_page => true)
-    @last_testimony = Testimony.limit(1).where(:landing_page => true)
+    @last_testimony = Testimony.last
     @health_random = Health.first
   end
 
